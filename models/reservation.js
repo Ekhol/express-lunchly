efc325de5b28bd97567a90d2416b47c9de16b881
Nathan <frost.nathan1995@gmail.com>
2022-10-30 15:37:49 -0400

    finished up to further study

--- models/reservation.js
+++ models/reservation.js
@@ -8,7 +8,7 @@ const db = require("../db");
 /** A reservation for a party */
 
 class Reservation {
-  constructor({id, customerId, numGuests, startAt, notes}) {
+  constructor({ id, customerId, numGuests, startAt, notes }) {
     this.id = id;
     this.customerId = customerId;
     this.numGuests = numGuests;
@@ -26,17 +26,74 @@ class Reservation {
 
   static async getReservationsForCustomer(customerId) {
     const results = await db.query(
-          `SELECT id, 
+      `SELECT id, 
            customer_id AS "customerId", 
            num_guests AS "numGuests", 
            start_at AS "startAt", 
            notes AS "notes"
          FROM reservations 
          WHERE customer_id = $1`,
-        [customerId]
+      [customerId]
     );
 
     return results.rows.map(row => new Reservation(row));
+  }
+
+  async save() {
+    if (this.id === undefined) {
+      const result = await db.query(
+        `INSERT INTO reservations (customer_id, num_guests, start_at, notes)
+          VALUES ($1, $2, $3, $4)
+          RETURNING id`,
+        [this.customerId, this.numGuests, this.startAt, this.notes]
+      );
+      this.id = result.rows[0].id;
+    } else {
+      await db.query(
+        `UPDATE reservations SET num_guests=$1, start_at=$2, notes=$3
+        WHERE id=$4`,
+        [this.numGuests, this.startAt, this.notes, this.id]
+      );
+    }
+  }
+
+  set notes(val) {
+    this._notes = val || "";
+  }
+
+  get notes() {
+    return this._notes;
+  }
+
+
+  set numGuests(val) {
+    if (val < 1)
+      throw new Error("Cannot reserve less than 1");
+    this._numGuests = val;
+  }
+
+  get numGuests() {
+    return this._numGuests;
+  }
+
+  set startAt(val) {
+    if (val instanceof Date && !isNaN(val))
+      this._startAt = val;
+    else throw new Error("Invalid Date");
+  }
+
+  get startAt() {
+    return this._startAt;
+  }
+
+  set customerId(val) {
+    if (this._customerId && this._customerId !== val)
+      throw new Error("Cannot change reservation owner");
+    this._customerId = val;
+  }
+
+  get custoemrId() {
+    return this._customerId;
   }
 }
 
